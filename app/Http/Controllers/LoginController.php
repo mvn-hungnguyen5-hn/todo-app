05095fd6ba1d091506580559087ad3605b32f76c
mvn-hungnguyen5-hn <hung.nguyen5@monstar-lab.com>
2022-05-11 22:52:29 +0700

    gg
    Signed-off-by: mvn-hungnguyen5-hn <hung.nguyen5@monstar-lab.com>

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -60,6 +60,7 @@ class LoginController extends Controller
         $user->email = $request->input('email');
         $user->level = 0;
         $user->password = bcrypt($request->input('password'));
+        //save
         $user->save();
         return redirect()->route('login');
     }
